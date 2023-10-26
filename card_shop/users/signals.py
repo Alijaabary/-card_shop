a1bef43f7b406cbb7809d10ed7457067dc5d14e2
user name <user email>
2023-10-26 23:11:08 +0200

    creat function to save user locatione in dbs

--- card_shop/users/signals.py
+++ card_shop/users/signals.py
@@ -9,3 +9,11 @@ from .models import Profile, Location
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        
+@receiver(post_save, sender=Profile)
+def create_profile_location(sender, instance, created, **kwrags):
+    if created:
+        profile_location = Location.objects.create()
+        instance.location = profile_location
+        instance.save()
+
